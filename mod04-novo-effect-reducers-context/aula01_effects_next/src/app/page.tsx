59f133c606d44579ee15e5d60c70ec7ab44c6b3c
Betoside <betoside@gmail.com>
2023-08-09 14:47:45 -0300

    #3: Usando o Effect da forma correta

--- mod04-novo-effect-reducers-context/aula01_effects_next/src/app/page.tsx
+++ mod04-novo-effect-reducers-context/aula01_effects_next/src/app/page.tsx
@@ -4,30 +4,23 @@ import { useEffect, useState } from 'react';
 
 const Page = () => {
 
-  // 1. Definição da função que vai rodar.
-  // 2. Definição de QUANDO vai rodar.
-  // 3. Definição do que fazer quando o componente sumir/unload.
-
-  const [name, setName] = useState('Boniek');
-  const [age, setAge] = useState(90);
-
-  useEffect(() => {
-    console.log('RODOU O EFFECT...');
-  }, [name]);
-
-  const handlePedroClick = ()=>{
-    setName('Pedro');
-  }
+  const [firstname, setFirstname] = useState<string>('Boniek');
+  const [lastname, setLastname] = useState<string>('Lacerd');
+  
+  const fullname = firstname + ' ' + lastname;
 
   return (
     <div className=''>
 
-      <p>Meu nome é {name}, com idade de {age}</p>
+      <p>Meu nome é {fullname}</p>
+      <hr />
 
-      <button className='p-2 border rounded-md m-3 border-blue-700' onClick={()=>{setName('Pedro')}}>Mudar para Pedro</button>
-      <button className='p-2 border rounded-md m-3 border-blue-700' onClick={()=>{setName('João')}}>Mudar para João</button>
-      <button className='p-2 border rounded-md m-3 border-blue-700' onClick={()=>{setAge(10)}}>Mudar para 10 anos</button>
-      <button className='p-2 border rounded-md m-3 border-blue-700' onClick={()=>{setAge(90)}}>Mudar para 90 anos</button>
+      <button className='p-2 border rounded-md m-3 border-blue-700' 
+        onClick={()=>{setFirstname('Pedro')}}
+      >Nome 'Pedro'</button>
+      <button className='p-2 border rounded-md m-3 border-blue-700' 
+        onClick={()=>{setLastname('Alvares')}}
+      >Sobrenome 'Alvares'</button>
 
     </div>
   )
