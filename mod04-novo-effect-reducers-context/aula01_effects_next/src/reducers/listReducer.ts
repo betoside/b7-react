7c9d873d946bc1d44284063f4352e53998f56577
Betoside <betoside@gmail.com>
2023-08-09 17:41:01 -0300

    #7: Criando um Reducer 1

---
+++ mod04-novo-effect-reducers-context/aula01_effects_next/src/reducers/listReducer.ts
@@ -0,0 +1,46 @@
+import { ItemTarefa } from "@/types/ItemTarefa";
+
+type AddAction = {
+    type: 'add';
+    payload: {
+        text: string
+    };
+}
+
+type EditTextAction = {
+    type: 'editText';
+    payload: {
+        id: number;
+        newText: string;
+    }
+}
+
+type ToggleDoneAction = {
+    type: 'toggleDone';
+    payload: {
+        id: number
+    };
+}
+
+type RemoveAction = {
+    type: 'remove';
+    payload: {
+        id: number
+    };
+}
+
+type ListActions = AddAction | EditTextAction | ToggleDoneAction | RemoveAction;
+
+export const listReducer = (list: ItemTarefa[], action: ListActions) => {
+
+    // executar alguma ação
+    // ao final retornar a lista atualizada
+    // por isso mando os dados originais 'list' e a ação a ser executada 'action'
+
+    // action.type // qual ação a ser executada
+    // action.payload // qual a carga dela, o dado que mando junto da ação
+
+
+    return list;
+
+}
