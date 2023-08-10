cbe3dcf573027e64e3453812f90dcd09e7f4707b
Betoside <betoside@gmail.com>
2023-08-09 22:12:50 -0300

    #9: Usando o Reducer 1

--- mod04-novo-effect-reducers-context/aula01_effects_next/src/app/page.tsx
+++ mod04-novo-effect-reducers-context/aula01_effects_next/src/app/page.tsx
@@ -8,48 +8,46 @@ import { useReducer, useState } from "react";
 const Page = () => {
 
   const [list, dispatch] = useReducer(listReducer, []);
+  const [addField, setAddField] = useState('');
 
-  const handleAddClick = () => {
+  const handleAddButton = () => {
+    if(addField.trim() === '') return false;
 
     dispatch({
       type: 'add',
       payload: {
-        text: 'Novo item'
+        text: addField.trim()
       }
     });
 
+    setAddField('');
   }
 
-  // dispatch({
-  //   type: "toggleDone",
-  //   payload: {
-  //     id: 2
-  //   }
-  // });
-
-  // dispatch({
-  //   type: 'editText',
-  //   payload: {
-  //     id: 2,
-  //     newText: 'Bla Bla Bla'
-  //   }
-  // });
-
-  // dispatch({
-  //   type: "remove",
-  //   payload: {
-  //     id: 2
-  //   }
-  // });
-
-
   return (
-    <div className=''>
+    <div className='container mx-auto text-white'>
 
-      <button
-        className="bg-blue-400 rounded-md p-3 text-black mb-2" 
-        onClick={handleAddClick}>Adicionar</button>
+      <h1 className="text-center text-4xl my-4">Lista de tarefas</h1>
+      <div className="border border-gray-400 p-4 m-4 bg-gray-900">
 
+        <input 
+          type="text" 
+          name="nota" 
+          value={addField}
+          onChange={e => setAddField(e.target.value)}
+          placeholder="Escreva uma tarefa"
+          className="p-3 mr-3 w-2/3 outline-none bg-transparent border rounded-lg"
+         />
+        <button
+          className="bg-blue-400 rounded-md p-3 text-black mb-2" 
+          onClick={handleAddButton}>Adicionar</button>
+
+        
+      </div>
+      <ul className="p-4 m-4">
+        {list.map((item) => (
+          <li key={item.id}>{item.id} - {item.text}</li>
+        ))}
+      </ul>
 
     </div>
   )
