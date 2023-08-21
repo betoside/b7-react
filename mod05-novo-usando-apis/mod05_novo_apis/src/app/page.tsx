ac226e5b734f6f9cd5112e967a32712b2867062b
Betoside <betoside@gmail.com>
2023-08-21 13:12:47 -0300

    feito: #9: Fazendo um Fetch POST

--- mod05-novo-usando-apis/mod05_novo_apis/src/app/page.tsx
+++ mod05-novo-usando-apis/mod05_novo_apis/src/app/page.tsx
@@ -28,8 +28,29 @@ export default function Home() {
 
   }, []);
 
+  const handleAddNewPost = async ()=>{
+    
+    const resposta = await fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      },
+      body: JSON.stringify({
+        title: "Post de Teste",
+        body: "Corpo de teste",
+        userId: 99
+      })
+    });
+
+    const json = await resposta.json();
+    console.log(json);
+
+  }
+
+
   return (
     <div className="container mx-auto p-6">
+      <button onClick={handleAddNewPost} className='border rounded-lg bg-gray-500 px-2 my-2'>Adicionar Novo Post</button>
       <h1 className="text-3xl">Lista de usuários</h1>
 
       {loading && "Carregando..."}
